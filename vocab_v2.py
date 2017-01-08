03c159493f8c9765d4ed02b157ab42ceb8f8511d
SomeUser <ashok05@somaiya.edu>
2017-01-08 16:30:48 +0530

    Added Search and improved code

--- vocab_v2.py
+++ vocab_v2.py
@@ -5,6 +5,8 @@ from random import randint
 prompt = 'y'
 score = 0
 count = 0
+stop = ''
+add_new = ''
 
 filename = 'vocab_append.txt'
 file_content = {}
@@ -14,7 +16,8 @@ word_list = {
 'COMATOSE'		: 'State of deep unconsciousness',
 'BELEAGUERED' 	: 'Put in a very difficult situation', 
 'SERMONIZING' 	: 'Deliver an opinionated and dogmatic talk',
-'SOLIPSISM'		: 'The quality of being self-centered and selfish'
+'SOLIPSISM'		: 'The quality of being self-centered and selfish',
+'SOMEWORD'		: 'SOME MEANING'
 }
 
 fp = open(filename,'w+')
@@ -27,15 +30,50 @@ print "Welcome to vocab!"
 
 #print "Meaning of the entered word is: ", word_list[word]
 
-choice = raw_input ("What would you like to do? \n\t[1]Add words\n\t[2]Guess meanings")
+choice = int(raw_input("What would you like to do? \n\t[1]Add words\n\t[2]Search a word\n\t[3]Guess meanings"))
 print "Choice registered: ", choice
 
-if(choice == 1):
-	#if body here
-	word_key = raw_input ("Word: ")
-	word_value = raw_input ("Meaning: ")
-	word_list.update({word_key:word_value})
-	print word_list
+if choice == 1:
+	while stop!='n':
+		word_key = raw_input ("Word: ")
+		word_value = raw_input ("Meaning: ")
+		word_list.update({word_key:word_value})
+		fp = open(filename,'a')
+		pickle.dump({word_key:word_value},fp)
+		fp.close()
+		with open(filename,'r+') as fp:
+			while 1:
+				try:
+					file_content.update(pickle.load(fp))
+				except EOFError:
+					break 
+			print "Total number of words: ",len(file_content)
+			for i in file_content:
+				print i,"\t: ",file_content[i]
+		stop = raw_input("Add more words? (y/n)")
+
+elif choice == 2:
+	search_word = raw_input("Word to be searched: ")
+	with open(filename,'r+') as fp:
+			while 1:
+				try:
+					file_content.update(pickle.load(fp))
+				except EOFError:
+					break 
+	if search_word.upper() in file_content:
+		print search_word.upper(),"\t: ",file_content[search_word.upper()]
+	else:
+		print "Apologies, word not found in the dictionary. Care to add it? (y/n)"
+		add_new = raw_input()
+		if add_new == 'y' or add_new == 'Y':
+			word_key = raw_input ("Word: ")
+			word_value = raw_input ("Meaning: ")
+			word_list.update({word_key:word_value})
+			fp = open(filename,'a')
+			pickle.dump({word_key:word_value},fp)
+			fp.close()
+		else:
+			print "Do look up for the word though!"
 
 else:
 	while (prompt!='n'):
